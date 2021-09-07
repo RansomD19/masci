079c6f75ebbf7aebdfff3ce271d66f34651d4e37
Rishabh Kichu <rishabhkkichu@gmail.com>
2021-09-07 18:17:23 +0300

    hi

--- index.js
+++ index.js
@@ -1,69 +1,69 @@
 const Discord = require("discord.js");
-const fetch = require("node-fetch")
-const client = new Discord.Client({intents:["GUILDS", "GUILD_MESSAGES"]});
+const fetch = require("node-fetch");
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 require("dotenv").config();
 
 const prefix = "m!";
 
-function getQoute(){
-    return fetch("https://zenqoutes.io/api/random")
-    .then(res => {
-        return res.json();
+function getQoute() {
+  return fetch("https://zenqoutes.io/api/random")
+    .then((res) => {
+      return res.json();
     })
-    .then(data => {
-        return data[0]["q"] + " -" + data[0]["a"];
-    })
+    .then((data) => {
+      return data[0]["q"] + " -" + data[0]["a"];
+    });
 }
 
 client.once("ready", () => {
-    console.log("[STATUS:BOT IS ONLINE]");
-    client.user.setPresence({
-        game:{
-            name:"MaSci",
-            type:"WATCHING" // LISTENING, PLAYING, STREAMING, WATCHING
-        },
-        status: "idle" // dnd, idle, invisible, online
-    })
-})
+  console.log("[STATUS:BOT IS ONLINE]");
+  client.user.setPresence({
+    game: {
+      name: "MaSci",
+      type: "WATCHING", // LISTENING, PLAYING, STREAMING, WATCHING
+    },
+    status: "idle", // dnd, idle, invisible, online
+  });
+});
 
-client.on("message", msg => {
-    if (
+client.on("message", (msg) => {
+  if (
     !msg.content.toLowerCase().startsWith(prefix.toLowerCase()) ||
     msg.author.bot
-    )
+  )
     return;
 
   const args = msg.content.slice(prefix.length).trim().split(" ");
   const cmd = args.shift().toLowerCase();
   console.log(cmd, args);
 
-     
-    if(cmd === "binary"){
-        msg.channel.send("Sap's Part")
+  if (cmd === "binary") {
+    msg.channel.send("Sap's Part");
+  }
+
+  if (cmd === "herons") {
+    if (args.length === 3) {
+    } else return msg.reply("Please provide 3 numbers");
+
+    if (isNaN(args[0]) || isNaN(args[1]) || isNaN(args[2])) {
+      msg.channel.send("Pls provide 3 numbers");
     }
 
-    if(cmd === "herons"){
-        if(args.length === 3){
-        } else return msg.reply("Please provide 3 numbers");
-
-        if(isNaN(args[0]) || isNaN(args[1]) || isNaN(args[2])){
-            msg.channel.send("Pls provide 3 numbers")
-        }
-        msg.reply(herons(args[0], args[1], args[2]));
+    if (args[0] + args[1] > args[2]) {
+      msg.reply(herons(args[0], args[1], args[2]));
+    } else {
+      return msg.channel.send("Pls make sure the a + b > c");
     }
+  }
 
-    if(cmd == "ping"){
-        msg.channel.send("Pinging to Servers....")
-        .then((message) => {
-            msg.edit("Ping" + (Date.now() - message.createdTimestamp))
-        });
-    }
+  if (cmd == "ping") {
+    msg.channel.send("Pinging to Servers....").then((message) => {
+      msg.edit("Ping" + (Date.now() - message.createdTimestamp));
+    });
+  }
+});
 
-
-})
-
-
-client.login(process.env.TOKEN)
+client.login(process.env.TOKEN);
 
 /*
 hello there 
